42d09592099f715717cf734c6b227ea58cbfe6a0
Renato Cervati <renato.cervati@gmail.com>
2019-07-26 21:42:21 -0300

    commit inicial

---
+++ src/php-classes/PageAdmin.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Controlic;
+
+class PageAdmin extends Page {
+
+	public function __construct($opts = array(), $tpl_dir = "/views/")
+	{
+
+		parent::__construct($opts, $tpl_dir);
+
+	}
+
+}
+
+ ?>
